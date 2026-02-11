cef26bf2a897cbd4c452138896b24994f33b5000
Liubov <kolyasa.lubov@gmail.com>
2026-02-11 18:06:34 +0200

    change code

--- hw07/lkoli/codewars.py
+++ hw07/lkoli/codewars.py
@@ -1,6 +1,6 @@
-# def greet(name):
-#     if name == "Johnny":
-#         return "Hello, my love!"
-#     return "Hello, {0}!".format(name)
-
+def greet(name):
+    if name == "Johnny":
+        return "Hello, my love!"
+    return "Hello, {0}!".format(name)
+#change the code above to return "Hello, my love!" if the name is "Johnny". For any other name, return "Hello, <name>!" where <name> is the name given.
 ###########################################3
