af1941eaea444cbe4c0a0eda5e51dc6221e0b592
Vitalii Yatsenko <inf.yatsenko@gmail.com>
2026-02-17 12:38:27 +0200

    Added code corrections to the task file

--- hw08/VitaliiYatsenko/password_validator.py
+++ hw08/VitaliiYatsenko/password_validator.py
@@ -7,7 +7,7 @@ template = r'^(?=.*[$#@])(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d$#@]{6,16}$'
 
 compile_template = re.compile(template)
 
-if compile_template.search(password):
+if compile_template.fullmatch(password):
     print('Your password was created')
 else:
     print('Your password does not meet security requirements.')
