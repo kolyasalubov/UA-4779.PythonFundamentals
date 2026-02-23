eb0b3012fb5037f361a85d144fc78a1725e605e8
Viacheslav Nepochatykh <slava2626@gmail.com>
2026-02-23 12:40:30 +0200

    v_nepochatykh/hw07 added codewars file

---
+++ hw07/v_nepochatykh/codewars.py
@@ -0,0 +1,72 @@
+# def greet(name):  
+#     if name == "Johnny":
+#         return "Hello, my love!"
+#     else:
+#         return f"Hello, {name}!"
+    
+#############################################
+
+# def distance(x1, y1, x2, y2):
+#     return (round(((x2-x1)**2+(y2-y1)**2)**0.5, 2))
+
+###############################################
+
+# def filter_words(st):
+#     st = st.split ()
+#     return  ' '.join (st).capitalize()  
+
+################################################
+
+# def number_to_string(num):
+#     return str(num)
+
+###################################################
+
+# def reverse(st):
+#     return ' '.join(st.split(' ')[::-1])
+
+#################################################
+
+# def reverse_list(l):
+#     l.reverse()
+#     return l
+
+####################################################
+
+# def solution(number):
+#     sum = 0
+#     if number > 0:
+#         for i in range (number):
+#             if i%3 == 0 or i%5 == 0:
+#                 sum += i       
+#     return sum
+
+####################################################
+# 
+# def zero_fuel(distance_to_pump, mpg, fuel_left):
+#     return distance_to_pump <= mpg*fuel_left
+
+####################################################
+#   
+# def are_you_playing_banjo(name):
+#     return f"{name} plays banjo" if name[0].lower() == 'r' else f"{name} does not play banjo"
+
+####################################################
+#       
+# def bool_to_word(boolean):
+#     return 'Yes' if boolean else 'No'
+
+####################################################
+
+# def count_sheeps(sheep):
+#     present = 0
+#     for element in sheep:
+#         if element:
+#             present +=1
+#     return present
+
+#######################################################
+
+# def correct_tail(body, tail):
+#     return True if body[-1] == tail else False
+
