4464e351157a28bfa06f9ec083f9796a4902d1d8
Siuzanna Pyrohova <sunsweetypie02@gmail.com>
2026-03-11 19:20:25 +0300

    added hw07

---
+++ hw07/Sunsweetpie02/Task01.py
@@ -0,0 +1,11 @@
+############################################
+
+def largest_number(a,b):
+    return a if a > b else b
+
+#example
+
+result = largest_number(15, 3)
+print(result)
+
+###########################################
